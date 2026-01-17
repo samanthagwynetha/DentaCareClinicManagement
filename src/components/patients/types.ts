f4694141425c255e47a1d34612b182d9d6f6177a
samanthagwynetha <s.arsua.541584@umindanao.edu.ph>
2026-01-17 23:45:47 +0800

    types

---
+++ src/components/patients/types.ts
@@ -0,0 +1,10 @@
+export type Patient = {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  birthDate?: string;
+  phone: string;
+  email: string;
+  address: string;
+};
